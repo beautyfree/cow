e5c065c461f97342b13fdcb440f2751544e3273b
beauty_free <felex-ae@ya.ru>
2012-05-19 14:34:11 +0400

    Кормим корову

--- src/com/ad/cow/Cow.java
+++ src/com/ad/cow/Cow.java
@@ -1,22 +1,27 @@
 package com.ad.cow;
 
-import com.ad.cow.R;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class Cow extends Activity {
+	private ProgressBar mProgress;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        mProgress = (ProgressBar) findViewById(R.id.progressBar1);
+        mProgress.setProgress(0);
     }
-    int satiety = 60;
+    
     public void feed(View view){
-    	if(satiety > 50){
+    	mProgress.setProgress(mProgress.getProgress()+10);
+    	if(mProgress.getProgress() > 50){
     		Toast.makeText(this, "Ваша корова сыта. Приходите когда она проголодается!",
                     Toast.LENGTH_LONG).show();
     		return;
