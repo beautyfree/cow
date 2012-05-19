2d2f1a0218eab40821017f1632bf9d6193e21dd8
beauty_free <felex-ae@ya.ru>
2012-05-20 00:25:13 +0400

    feed by time

--- src/com/ad/cow/Cow.java
+++ src/com/ad/cow/Cow.java
@@ -1,5 +1,7 @@
 package com.ad.cow;
 
+import java.util.Date;
+
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -12,7 +14,10 @@ public class Cow extends Activity {
 	private SharedPreferences mySharedPreferences;
 	private static String MY_PREFS = "MY_PREFS";
 	
-	private int percent;
+	private double perMinute = 0.083;
+	private double percentByFood = 1.2;
+	private int  percent;
+	private long time;
 	
     /** Called when the activity is first created. */
     @Override
@@ -25,9 +30,20 @@ public class Cow extends Activity {
     
     private void loadPreferences() {
     	int mode = Activity.MODE_PRIVATE;
+    	long currentTime = new Date().getTime();
     	
-    	mySharedPreferences = getSharedPreferences(MY_PREFS,mode);
-    	percent = mySharedPreferences.getInt("percent",0);
+    	mySharedPreferences = getSharedPreferences(MY_PREFS, mode);
+    	percent = mySharedPreferences.getInt("percent", 0);
+    	time    = mySharedPreferences.getLong("time", currentTime);
+    	
+    	long diff = currentTime - time;
+    	double minutes = diff / 1000 / 60;
+    	
+    	double eatenFood = minutes * perMinute;
+    	int cutPercent = (int) Math.round(eatenFood / percentByFood);
+    	int newPercent = percent - cutPercent;
+    	
+    	percent = Math.max(0, newPercent);
     	
     	mProgress = (ProgressBar) findViewById(R.id.progressBar1);
         mProgress.setProgress(percent);
@@ -39,15 +55,15 @@ public class Cow extends Activity {
     	if(newPercent <= 100) {
 	    	SharedPreferences.Editor editor = mySharedPreferences.edit();
 	    	editor.putInt("percent",newPercent);
+	    	editor.putLong("time",new Date().getTime());
 	    	editor.commit();
 	    	
 	    	mProgress.setProgress(newPercent);
     	}
     	
     	if(newPercent > 50){
-    		Toast.makeText(this, "Ваша корова сыта. Приходите когда она проголодается!",
-                    Toast.LENGTH_LONG).show();
-    		return;
+    		Toast.makeText(this, "Ваша корова сыта. Приходите когда она проголодается!", 
+    				Toast.LENGTH_LONG).show();
     	}
     }
 }
