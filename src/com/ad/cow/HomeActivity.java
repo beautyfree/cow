61654815b86d863e0cb7c08516918f2f7c59b9cf
beauty_free <felex-ae@ya.ru>
2012-05-23 02:57:38 +0400

    docs and resurrect

--- src/com/ad/cow/HomeActivity.java
+++ src/com/ad/cow/HomeActivity.java
@@ -4,18 +4,25 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+/**
+ * Главная страница
+ *
+ */
 
 public class HomeActivity extends AbstractActivity {
+	/**
+	 * Необходимые переменные
+	 */
 	private final String MY_PREFS = "MY_PREFS";
 
 	private ProgressBar mProgress;
@@ -31,24 +38,32 @@ public class HomeActivity extends AbstractActivity {
 	private float percent;
 	private long time;
 	
-	
-	/** Called when the activity is first created. */
+	/**
+	 * Старт активности
+	 */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-
+		
 		loadPreferences();
 	}
 
+	/**
+	 * Загружаем настройки и проводим необходимые вычисления
+	 */
 	private void loadPreferences() {
 		int mode = Activity.MODE_PRIVATE;
 		long currentTime = new Date().getTime();
-
+		
+		textView = (TextView) findViewById(R.id.textView1);
+		
+		// Достаем сохраненные данные
 		mySharedPreferences = getSharedPreferences(MY_PREFS, mode);
 		percent = mySharedPreferences.getFloat("percentf", 10.0f);
 		time = mySharedPreferences.getLong("time", currentTime);
 
+		// Проводим вычисления процента
 		long diff = currentTime - time;
 		float seconds = diff / 1000;
 		float eatenFood = seconds * perSecond;
@@ -58,28 +73,36 @@ public class HomeActivity extends AbstractActivity {
 
 		percent = Math.max(0, newPercent);
 
+		// Устанавливаем отметку на прогрессбаре голода
 		mProgress = (ProgressBar) findViewById(R.id.progressBar1);
 		mProgress.setProgress((int) percent);
 
-		textView = (TextView) findViewById(R.id.textView1);
-
+		// Запускаем таймер голода коровки
 		long timer = (long) ((percent * percentByFood) / perSecond) * 1000;
 		countDownTimer = new FeedCountDownTimer(timer, interval);
 		countDownTimer.start();
 		
+		// Создаем всплывающее сообщение для дальнейшего использования
+		// о том что коровка сыта
 		toast = Toast.makeText(this,
 				R.string.cowfeed,
 				Toast.LENGTH_LONG
 		);
 	}
     
+	/**
+	 * Метод выполняющийся при нажатии кнопки Покормить
+	 * @param view
+	 */
 	public void onFeedClicked(View view) {		
 		/*
+		 * Показываем ачивку
 		Intent intent = new Intent(this, AchieveActivity.class);
 		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 		startActivity(intent);
 		*/
 		
+		// Вычисляем новое значение процента голода
 		int newPercent = mProgress.getProgress() + 10;
 
 		if (newPercent <= 100) {
@@ -88,58 +111,85 @@ public class HomeActivity extends AbstractActivity {
 			newPercent = 100;
 			percent = 100;
 		}
+		mProgress.setProgress(newPercent);
 		
+		// Сохраняем измененные данные
 		SharedPreferences.Editor editor = mySharedPreferences.edit();
 		editor.putFloat("percentf", percent);
 		editor.putLong("time", new Date().getTime());
 		editor.commit();
 
-		mProgress.setProgress(newPercent);
-
+		// Останавливаем предыдущий таймер и стартуем с новыми данными
 		countDownTimer.cancel();
 		long timer = (long) ((percent * percentByFood) / perSecond) * 1000;
 		countDownTimer = new FeedCountDownTimer(timer, interval);
 		countDownTimer.start();
 
+		// Если процент голода больше 50 выводим всплывающее сообщение
+		// о том что коровка сыта
 		if (newPercent > 50) {
 			toast.cancel();
 			toast.show();
 		}
 	}
 
+	/**
+	 * Класс таймера обратного отсчета голода коровки
+	 *
+	 */
 	private class FeedCountDownTimer extends CountDownTimer {
 		public FeedCountDownTimer(long startTime, long interval) {
 			super(startTime, interval);
 		}
 
+		/**
+		 * При истечении времени коровка погибает
+		 */
 		@Override
 		public void onFinish() {
 			Die();
 		}
 
+		/**
+		 * Выполняет при каждом наступлении интервала
+		 */
 		@Override
 		public void onTick(long millisUntilFinished) {
 			percent = (millisUntilFinished / 1000) * perSecond / percentByFood;
 
+			// Устанавливаем новый процент голода коровки
 			int newPercent = (int) Math.round(percent);
 			mProgress.setProgress(newPercent);
 
+			// Задаем формат для оставшегося времени
 			SimpleDateFormat sdf = new SimpleDateFormat("mm:ss");
 			Date resultdate = new Date(millisUntilFinished);
 
+			// Узнаем общшее количество оставшихся часов
 			long hours = millisUntilFinished / 1000 / 60 / 60;
 			textView.setText(getString(R.string.goback) + ": " + hours + ":"
 					+ sdf.format(resultdate));
 		}
 	}	
     
+	/**
+	 * Метод наступления смерти коровки
+	 */
 	private void Die() {
+		// Надпись коровка погибла
 		textView.setText(R.string.cowdie);
 		
+		// Изображение мертвой коровки
 		ImageView cow = (ImageView) findViewById(R.id.imageView1);
 		cow.setImageResource(R.drawable.cowdie);
+		
+		Button resurrect = (Button) findViewById(R.id.button1);
+		resurrect.setText(R.string.resurrect);
 	}
 	
+	/**
+	 * При завершении экшена сохраняем данные
+	 */
 	@Override
 	protected void onStop() {
 		super.onStop();
