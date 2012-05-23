127088dd1b05a739f75d6fb53f2ae9a11f44762c
Denis Lyk'yanov <kakawka05@gmail.com>
2012-05-24 01:14:24 +0400

    menu

---
+++ src/com/ad/cow/ExperienceActivity.java
@@ -0,0 +1,13 @@
+package com.ad.cow;
+
+import android.os.Bundle;
+
+public class ExperienceActivity extends AbstractActivity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.experience);
+	}
+}
