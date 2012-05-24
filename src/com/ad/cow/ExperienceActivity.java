b6d288c5e9795f0518db03d04669f03367dc1e8e
Denis Lyk'yanov <kakawka05@gmail.com>
2012-05-24 15:04:57 +0400

    expierence

--- src/com/ad/cow/ExperienceActivity.java
+++ src/com/ad/cow/ExperienceActivity.java
@@ -1,13 +1,52 @@
 package com.ad.cow;
 
+import java.util.Date;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
 
 public class ExperienceActivity extends AbstractActivity {
 
+	private long time;
+	private final String MY_PREFS = "MY_PREFS";
+	private int exp;
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.experience);
+		
+		loadPreferences();
+		
+		}
+	public void loadPreferences(){
+		int mode = Activity.MODE_PRIVATE;
+		
+		long currentTime = new Date().getTime();
+		
+		SharedPreferences mySharedPreferences = getSharedPreferences(MY_PREFS, mode);
+		time = mySharedPreferences.getLong("time", currentTime);
+		
+		long diff = currentTime - time;
+		float hours = diff / 3600000;
+		long expPerHour = (long) (hours * 10);
+
+		TextView textView = (TextView) findViewById(R.id.textView1);
+		textView.setText("У вас " + hours + " опыта");
+		
+		int percent = 60;
+		ProgressBar progressView = (ProgressBar) findViewById(R.id.progressBar1);
+		progressView.setProgress((int)percent);
+		
+		
+		//сохраняем данные
+		SharedPreferences.Editor editor = mySharedPreferences.edit();
+	    editor.putFloat("exp", exp);
+	    editor.putLong("time", new Date().getTime());
+	    editor.commit();
 	}
 }
